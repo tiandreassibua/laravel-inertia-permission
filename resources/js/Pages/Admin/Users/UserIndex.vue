3670b6af0595f8ff89a3cd254ed972ac23fbc6c8
Hinata <hinata@pop-os.localdomain>
2023-08-27 03:33:21 +0700

    add modal confirmation on delete user

--- resources/js/Pages/Admin/Users/UserIndex.vue
+++ resources/js/Pages/Admin/Users/UserIndex.vue
@@ -1,13 +1,33 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 
+import Modal from "@/Components/Modal.vue";
+import DangerButton from "@/Components/DangerButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 import Table from "@/Components/Table.vue";
 import TableRow from "@/Components/TableRow.vue";
 import TableHeaderCell from "@/Components/TableHeaderCell.vue";
 import TableDataCell from "@/Components/TableDataCell.vue";
+import { ref } from "vue";
 
 defineProps(["users"]);
+const form = useForm({});
+
+const deleteUser = (userId) => {
+    form.delete(route("users.destroy", userId), {
+        onSuccess: () => closeModal(),
+    });
+};
+
+const showConfirmDeleteUserModel = ref(false);
+
+const closeModal = () => {
+    showConfirmDeleteUserModel.value = false;
+};
+const confirmDeleteUser = () => {
+    showConfirmDeleteUserModel.value = true;
+};
 </script>
 
 <template>
@@ -49,13 +69,42 @@ defineProps(["users"]);
                                     class="text-green-500 font-semibold hover:text-green-600"
                                     >Edit</Link
                                 >
-                                <Link
+                                <!-- <Link
                                     :href="route('users.destroy', user.id)"
                                     method="delete"
                                     as="button"
                                     class="text-red-500 font-semibold hover:text-red-600"
                                     >Delete</Link
+                                > -->
+                                <button
+                                    @click="confirmDeleteUser"
+                                    class="text-red-400 hover:text-red-600"
                                 >
+                                    Delete
+                                </button>
+                                <Modal
+                                    :show="showConfirmDeleteUserModel"
+                                    @close="closeModal"
+                                >
+                                    <div class="p-6">
+                                        <h2
+                                            class="text-lg font-semibold text-slate-800"
+                                        >
+                                            Are you sure to delete this user?
+                                        </h2>
+                                        <div
+                                            class="mt-6 flex justify-end space-x-4"
+                                        >
+                                            <DangerButton
+                                                @click="deleteUser(user.id)"
+                                                >Delete</DangerButton
+                                            >
+                                            <SecondaryButton @click="closeModal"
+                                                >Cancel</SecondaryButton
+                                            >
+                                        </div>
+                                    </div>
+                                </Modal>
                             </TableDataCell>
                         </TableRow>
                         <TableRow v-else>
